2fd512e5f00e04305f89f8514183efaffff7cc73
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-02 11:31:39 -0300

    Adicionando library e docs da forma correta em Intellij ao clicar em Project Structure/ modules e Libraries

--- src/com/company/bibliotecaemail/UsandoBibliotecaEmail.java
+++ src/com/company/bibliotecaemail/UsandoBibliotecaEmail.java
@@ -1,8 +1,23 @@
 package com.company.bibliotecaemail;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+
+
 public class UsandoBibliotecaEmail {
 
     public static void main (String [] args){
+        Email email = new SimpleEmail();
+        email.setHostName("smtp.googlemail.com");
+        email.setSmtpPort(465);
+        email.setAuthenticator(new DefaultAuthenticator("username", "password"));
+        email.setSSLOnConnect(true);
+        email.setFrom("user@gmail.com");
+        email.setSubject("TestMail");
+        email.setMsg("This is a test mail ... :-)");
+        email.addTo("foo@bar.com");
+        email.send();
 
     }
 }
