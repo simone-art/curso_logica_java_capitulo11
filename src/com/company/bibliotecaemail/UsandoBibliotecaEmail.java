dce1458cbea395e6f78e648dc1fd69ca21505fc3
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-02 11:35:10 -0300

    Incluido no static main o throws EmailException para parar de dar erro

--- src/com/company/bibliotecaemail/UsandoBibliotecaEmail.java
+++ src/com/company/bibliotecaemail/UsandoBibliotecaEmail.java
@@ -2,12 +2,13 @@ package com.company.bibliotecaemail;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 
 
 public class UsandoBibliotecaEmail {
 
-    public static void main (String [] args){
+    public static void main (String [] args) throws EmailException {
         Email email = new SimpleEmail();
         email.setHostName("smtp.googlemail.com");
         email.setSmtpPort(465);
