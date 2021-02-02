41dd26f718cc513f303cfaa3c7303d9628dac2cd
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-02 14:01:12 -0300

    Código ainda sem funcionar porque está dando um erro no DataHandler

--- src/main/java/com/company/bibliotecaemail/UsandoBibliotecaEmail.java
+++ src/main/java/com/company/bibliotecaemail/UsandoBibliotecaEmail.java
@@ -9,20 +9,20 @@ import org.apache.commons.mail.SimpleEmail;
 public class UsandoBibliotecaEmail {
 
     public static void main (String [] args) throws EmailException {
-        Email email = new SimpleEmail();
-        //smtp é o servidor do smtp do gmail
-        email.setHostName("smtp.googlemail.com");
-        //setSmtpPort(465) é a porta do servidor
-        email.setSmtpPort(465);
-        email.setAuthenticator(new DefaultAuthenticator("digitalnotebook8", "cursologicajava77"));
-        email.setSSLOnConnect(true);
-        email.setFrom("digitalnotebook8@gmail.com");
-        email.setSubject("Aula sobre uso de bibliotecas e libraries");
-        email.setMsg("Isto é um teste... :-)");
-        email.addTo("simoneperiodista3@gmail.com");
-        email.send();
 
-        System.out.println("Fim");
 
+            Email email = new SimpleEmail();
+            //smtp é o servidor do smtp do gmail
+            email.setHostName("smtp.googlemail.com");
+            //setSmtpPort(465) é a porta do servidor
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator("digitalnotebook8", "cursologicajava77"));
+            email.setSSLOnConnect(true);
+            email.setFrom("digitalnotebook8@gmail.com");
+            email.setSubject("Aula sobre uso de bibliotecas e libraries");
+            email.setMsg("Isto é um teste... :-)");
+            email.addTo("simoneperiodista3@gmail.com");
+            email.send();
+            System.out.println("Fim");
     }
 }
